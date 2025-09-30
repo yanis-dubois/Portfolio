b3172158a57969f3c880490d757074d23eb3665d
yanis-dubois <ianis.dubois@free.fr>
2025-09-30 17:00:50 +0200

    change to next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/Portfolio/' : '',
+  basePath: isProd ? '/Portfolio' : '',
+  output: 'export'
 };
 
 export default nextConfig;
