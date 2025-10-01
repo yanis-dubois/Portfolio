948080e028e003c588d006c784df3a039fcec42b
yanis-dubois <ianis.dubois@free.fr>
2025-10-01 12:04:21 +0200

    Updates

---
+++ _next/static/2ouo8K7DFWUBVLHmzl8IZ/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/37bc1e4e7cf51b16.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Portfolio//_next/:path+",
+        "destination": "/Portfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/chunks/7a1ceb56c3e70cfe.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/e2ad21f8f9a351dc.js",
-  "static/chunks/turbopack-04dcb27960f14e67.js"
-])
